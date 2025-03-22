3e75b2af7c15487cbf304f1b682023129447fffc
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-03-21 22:20:14 -0600

    first commit

---
+++ yv.sh
@@ -0,0 +1,20 @@
+if [ "$1" == "new" ]; then
+  echo "Creando nuevo proyecto..."
+
+  if [ -z "$2" ]; then
+    echo "No se ha especificado el nombre del proyecto"
+    exit 1
+  else
+    mkdir $2
+    cd $2
+    git init
+    touch README.md
+    echo "# " >>README.md
+    git add .
+    git commit -m "Initial commit"
+    echo "Proyecto creado con éxito"
+  fi
+
+else
+  echo "No hay argumentos"
+fi
