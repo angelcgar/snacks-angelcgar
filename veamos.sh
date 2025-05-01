c326d7f6e349809b433b37c3797491c68c345995
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-05-01 09:46:53 -0600

    feat: agregar soporte para usar eslint con el comando --use-eslint

--- veamos.sh
+++ veamos.sh
@@ -13,7 +13,60 @@ tercera_funcion() {
 }
 
 funcion_helper_1() {
-  primera_funcion
+  local nombre_proyecto=${1:-$NOMBRE_PROYECTO}
+  primera_funcion "$nombre_proyecto"
+  echo "donde estamos?"
+  pwd
+  # Supongo que aquí se puede empezar a cocinar con el proyecto, verdad?
+  echo "Instalando Eslint"
+  pnpm install -D eslint prettier eslint-plugin-react eslint-config-prettier eslint-plugin-prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser
+  echo "Configurando Eslint"
+  cat >eslint.config.js <<EOF
+import globals from "globals"; import react from "eslint-plugin-react"; import reactHooks from "eslint-plugin-react-hooks"; import reactRefresh from "eslint-plugin-react-refresh"; import tseslint from "typescript-eslint"; import prettier from "eslint-config-prettier"; import eslintPluginPrettier from "eslint-plugin-prettier"; export default tseslint.config({ ignores: ["dist", "node_modules"] }, { files: ["**/*.{ts,tsx}"], languageOptions: { parser: tseslint.parser, ecmaVersion: 2020, sourceType: "module", globals: globals.browser, parserOptions: { ecmaFeatures: { jsx: true } } }, plugins: { react, "@typescript-eslint": tseslint.plugin, "react-hooks": reactHooks, "react-refresh": reactRefresh, prettier: eslintPluginPrettier }, rules: { ...react.configs.recommended.rules, ...reactHooks.configs.recommended.rules, "react/jsx-uses-react": "off", "react/react-in-jsx-scope": "off", "react/jsx-uses-vars": "error", "react/display-name": "off", "react/prop-types": "off", "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], "@typescript-eslint/explicit-module-boundary-types": "off", "react-refresh/only-export-components": ["warn", { allowConstantExport: true }], "prettier/prettier": "error" } }, { rules: {}, settings: { react: { version: "detect" } } }, { ...prettier });
+EOF
+  echo "Configurando prettier"
+  touch .prettierrc
+  cat >.prettierrc <<EOF
+  { "semi": true, "singleQuote": true, "printWidth": 80, "tabWidth": 2, "trailingComma": "es5" }
+EOF
+  echo "Configurando prettier ignore"
+  touch .prettierignore
+  cat >.prettierignore <<EOF
+# Ignora la carpeta de salida de Vite
+dist
+build
+
+# Dependencias
+node_modules
+
+# Archivos de lock
+package-lock.json
+pnpm-lock.yaml
+yarn.lock
+
+# Configuraciones del entorno
+.env
+.env.*.local
+
+# Salidas de prueba o cobertura
+coverage
+*.log
+
+# Cache de herramientas
+.vite
+.eslintcache
+EOF
+
+  echo "Configurando eslint ignore"
+  cat >.eslintignore <<EOF
+# Ignora la carpeta de salida de Vite
+dist
+build
+# Dependencias
+node_modules
+EOF
+
+  echo "Todo listo para programar"
 }
 
 show_help() {
@@ -21,8 +74,13 @@ show_help() {
 }
 
 case "$1" in
-"--parametro")
-  funcion_helper_1
+"--use-eslint")
+  if [ -z "$2" ]; then
+    echo "Error: Se requiere un nombre de proyecto después de --use-eslint"
+    exit 1
+  fi
+  usar_eslint=true
+  funcion_helper_1 "$2"
   echo "Hola desde la función helper 1"
   ;;
 "")
