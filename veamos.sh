042dcd786942681375034c2bbd8e48d2b58ebf66
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-05-01 08:25:26 -0600

    feat: esto es un template para empezar a hacer bash script de forma escalable
    - no es el mejor del mudo pero sirve para tareas sencillas y repetidas.
    - trataba de sapararlo en modulos pero creo que no me a salido del todo
    bien, algo es algo, que sea escalable, esa es la palabra correcta.
    - dios la IA si que ayuda, no solo para terminar lo antes posible a
    programar, sino que tambien ayuda para aprender :)

---
+++ veamos.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+primera_funcion() {
+  echo "Hola desde la primera función"
+}
+
+segunda_funcion() {
+  echo "Hola desde la segunda función"
+}
+
+tercera_funcion() {
+  echo "Hola desde la tercera función"
+}
+
+funcion_helper_1() {
+  primera_funcion
+}
+
+show_help() {
+  echo "Uso: $0 [opciones]"
+}
+
+case "$1" in
+"--parametro")
+  funcion_helper_1
+  echo "Hola desde la función helper 1"
+  ;;
+"")
+  echo "Hola desde la función por defecto"
+  ;;
+*)
+  show_help
+  ;;
+esac
