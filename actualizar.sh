318689cdfd80321db80198b96e98c0b0696571fa
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-09-02 11:42:06 -0600

    feat: añadir paquetes que no uso al script

--- actualizar.sh
+++ actualizar.sh
@@ -2,40 +2,49 @@
 
 # Lista de paquetes que NO se deben actualizar (IDEs, navegadores, herramientas pesadas, etc.)
 EXCLUDE_PKGS=(
-    brave-bin
-    firefox
-    intellij-idea-community-edition
-    thunderbird
-    visual-studio-code-bin
-    libreoffice
-    docker
-    docker-compose
-    gcc
-    git
-    github-cli
-    go
-    jdk21-openjdk
-    jdk-openjdk
-    kdenlive 
-    kdeconnect
-    libreoffice-fresh
-    libreoffice-fresh-es
-    okular
-    virtualbox
-    virtualbox-host-modules-arch
-    vlc
-    warp-terminal 
-    wine
-    cloudflared
-    containerd
-    snapd
+  #    brave-bin
+  firefox
+  intellij-idea-community-edition
+  thunderbird
+  #    visual-studio-code-bin
+  libreoffice
+  docker
+  docker-compose
+  #    gcc
+  git
+  github-cli
+  go
+  jdk21-openjdk
+  jdk-openjdk
+  kdenlive
+  kdeconnect
+  libreoffice-fresh
+  libreoffice-fresh-es
+  okular
+  virtualbox
+  virtualbox-host-modules-arch
+  vlc
+  warp-terminal
+  wine
+  cloudflared
+  cloudflare-warp-bin
+  containerd
+  snapd
+  postman-bin
+  insomnia
+  electron36
+  electron37
+  mongodb-compass
+  gitkraken
 )
 
 # Generar la cadena de exclusión para pacman
 EXCLUDE_STRING=$(printf " --ignore %s" "${EXCLUDE_PKGS[@]}")
 
+# echo $EXCLUDE_STRING
+
 # Ejecutar actualización con exclusión de paquetes pesados
 echo "Actualizando el sistema, excluyendo paquetes no esenciales..."
-sudo pacman -Syu $EXCLUDE_STRING #--noconfirm
+echo "pacman -Syu $EXCLUDE_STRING" #--noconfirm
 
 echo "¡Actualización completada!"
